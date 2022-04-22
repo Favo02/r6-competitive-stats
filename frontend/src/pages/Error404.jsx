2b43ced92467e7e2025ae282658171037d8741fc
Luca Favini <luca.favini.02@gmail.com>
2022-04-22 23:45:57 +0200

    Frontend: CSS: error 404 page css

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -1,6 +1,10 @@
-import { Navigate } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 
 import Header from "../components/Header"
+
+import classnames from "classnames"
+import CommonStyles from "../styles/common.module.scss"
+import Error404Styles from "./Error404.module.scss"
 
 const Error404 = ({ user, logout }) => {
 
@@ -11,9 +15,30 @@ const Error404 = ({ user, logout }) => {
     }
 
     return (
-        <div>
+        <div className={Error404Styles.background}>
             <Header user={user} logout={logout} />
-            <h1>Page not found - error 404</h1>
+
+            <div className={Error404Styles.center}>
+                <div
+                    // Added invisible char: the glitch effect does not work on the last word
+                    className={Error404Styles.glitch}
+                    data-text="Oops Page not found&zwnj;"
+                >Oops Page <span className={CommonStyles.highlighted}>not found</span> &zwnj;</div>
+                <div
+                    className={Error404Styles.glitch}
+                    data-text="Error 404 &zwnj;"
+                >Error 404 &zwnj;</div>
+                <h3 className={Error404Styles.text}>
+                    It looks like that Mute jammed this page, we could not find it :&#40;
+                </h3>
+                <Link
+                    className={classnames(
+                        CommonStyles.highlighLinkButton,
+                        Error404Styles.button
+                    )}
+                    to="/home"
+                >Go back to home</Link>
+            </div>
         </div>
     )
 }
