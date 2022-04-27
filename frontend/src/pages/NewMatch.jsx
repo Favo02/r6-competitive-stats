af132f294b3754516323e844947578c9d2596342
Luca Favini <luca.favini.02@gmail.com>
2022-04-27 12:01:53 +0200

    Frontend: create match form

--- frontend/src/pages/NewMatch.jsx
+++ frontend/src/pages/NewMatch.jsx
@@ -1,35 +1,32 @@
 import Header from "../components/Header"
-import FileUploader from "../components/FileUploader"
+import MatchForm from "../components/MatchForm"
 
-import React, { useState } from "react"
+import matchService from "../services/matches"
 
 const NewMatch = ({ user, logout }) => {
-    const [parsedData, setParsedData] = useState(null)
+    const newMatch = async (match) => {
+        const tempmatch = {
+            info: match.info,
+            performance: match.performance
+        }
 
-    window.onbeforeunload = function(e) {
-        if (parsedData) {
-            const dialogText = "Match not saved yet. Data will be lost"
-            e.returnValue = dialogText
-            return dialogText
+        if (match) {
+            try {
+                const createdMatch = await matchService.create(tempmatch, user.token)
+                console.log("saved match:", createdMatch)
+            }
+            catch (exception) {
+                if (exception.response) {
+                    console.log("Error", exception.response.status, ":", exception.response.data.error)
+                }
+            }
         }
     }
 
     return (
         <div>
             <Header user={user} logout={logout} />
-            <FileUploader setParsedData={setParsedData} />
-            { parsedData
-                ?
-                <>
-                    <h1>Match caricato:</h1>
-                    <h2>{parsedData.info.map.charAt(0).toUpperCase() + parsedData.info.map.slice(1)} - {parsedData.info.date.toLocaleDateString("it-IT")}: {parsedData.result.my_team.score} - {parsedData.result.enemy_team.score}</h2>
-                    <button>Salva match</button>
-                </>
-                :
-                <>
-                    <h1>Nessun match caricato</h1>
-                </>
-            }
+            <MatchForm newMatch={newMatch} />
         </div>
     )
 }
