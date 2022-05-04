bde9dd053e57d54e8357d0231e1ee4482b4999ef
Luca Favini <luca.favini.02@gmail.com>
2022-05-04 18:46:31 +0200

    Frontend: loading while fetching matches in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,20 +3,25 @@ import { useState, useEffect } from "react"
 import matchService from "../services/matches"
 
 import Header from "../components/common/Header"
+import Loading from "../components/common/Loading"
 import Matches from "../components/home/Matches"
 
 const Home = ({ user, logout }) => {
+    const [loading, setLoading] = useState(true)
     const [matches, setMatches] = useState([])
 
     useEffect(() => {
         try {
+            setLoading(true)
             matchService
                 .getAll(user.token)
                 .then(matches =>
                     setMatches(matches.sort((a, b) => a.info.date > b.info.date ? -1 : 1))
                 )
+            setLoading(false)
         }
         catch (exception) {
+            setLoading(false)
             console.log(exception)
             if (exception.response) {
                 console.log("Error", exception.response.status, ":", exception.response.data.error)
@@ -37,6 +42,8 @@ const Home = ({ user, logout }) => {
     return (
         <div>
             <Header user={user} logout={logout} />
+
+            {loading && <Loading />}
             <Matches matches={matches} deleteMatch={deleteMatch} />
         </div>
     )
