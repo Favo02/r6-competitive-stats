d663eb2edbab4292c11eb93b4d73d5043ad1318f
Luca Favini <luca.favini.02@gmail.com>
2022-05-05 18:42:13 +0200

    Backend: bug fixes: creating a team doens't add the creator as admin to the team

--- backend/controllers/teams.js
+++ backend/controllers/teams.js
@@ -2,10 +2,21 @@ const teamsRouter = require("express").Router()
 const Team = require("../models/team")
 const middleware = require("../utils/middleware")
 
-teamsRouter.post("/", middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
-    const team = request.body
+teamsRouter.get("/", middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
     const user = request.user
-    team.users = [{ id: user.id, permission: "admin" }]
+
+    const teams = await Team
+        .find({ "member.id": user.id })
+
+    response.json(teams)
+})
+
+teamsRouter.post("/", middleware.tokenExtractor, middleware.userExtractor, async (request, response) => {
+    const user = request.user
+    const team = {
+        name: request.body.name,
+        members: [{ id: user.id, permission: "admin" }]
+    }
 
     const existingTeam = await Team.findOne({ "name": team.name })
     if (existingTeam) {
