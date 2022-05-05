5a90a566b2a5ef08e63f27c57b64ed1515eda3bb
Luca Favini <luca.favini.02@gmail.com>
2022-05-06 01:49:38 +0200

    Backend: removed from api get every user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,13 +2,6 @@ const bcrypt = require("bcrypt")
 const usersRouter = require("express").Router()
 const User = require("../models/user")
 
-usersRouter.get("/", async (request, response) => {
-    const users = await User
-        .find({})
-
-    response.json(users)
-})
-
 usersRouter.post("/", async (request, response) => {
     const { username, password } = request.body
 
