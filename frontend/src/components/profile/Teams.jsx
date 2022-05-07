66b1aeb12ad58d6a5461c4509898f7fc6c40e550
Luca Favini <luca.favini.02@gmail.com>
2022-05-07 15:50:05 +0200

    Frontend: members and waiting members display on teams in profile page

--- frontend/src/components/profile/Teams.jsx
+++ frontend/src/components/profile/Teams.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 import teamService from "../../services/teams"
 
@@ -13,11 +13,27 @@ const Teams = ({ user }) => {
                 .getAll(user.token)
                 .then(teams => {
                     setTeams(
-                        teams.map(t => ({
-                            name: t.name,
-                            id: t.id,
-                            permission: t.members.find(m => m.id === user.id).permission
-                        }))
+                        teams.map(t => {
+                            return ({
+                                name: t.name,
+                                id: t.id,
+                                nMatches: t.matches.length,
+                                permission: t.members.find(m => m.id.id === user.id).permission,
+                                members: t.members.find(m => m.id.id === user.id).permission === "admin"
+                                    ? t.members.map(m => ({
+                                        id: m.id.id,
+                                        username: m.id.username,
+                                        permission: m.permission
+                                    }))
+                                    : null,
+                                waitingMembers: t.members.find(m => m.id.id === user.id).permission === "admin"
+                                    ? t.waitingMembers.map(m => ({
+                                        id: m.id,
+                                        username: m.username
+                                    }))
+                                    : null
+                            })
+                        })
                     )
                 })
         }
@@ -32,7 +48,19 @@ const Teams = ({ user }) => {
     return (
         <>
             <h1>Your teams:</h1>
-            {teams.map(t => <h1 key={t.name}>{t.name}: {t.permission}</h1>)}
+            {teams.map(t => {
+                return (
+                    <React.Fragment key={t.id}>
+                        <h1>{t.name}</h1>
+                        <h2>Members:</h2>
+                        {t.members.map(m => <h3 key={m.id}>{m.username} - {m.permission}</h3>)}
+                        <h2>Waiting members:</h2>
+                        {t.waitingMembers.map(m => <h3 key={m.id}>{m.username}</h3>)}
+                        <br />
+                        <br />
+                    </React.Fragment>
+                )
+            })}
         </>
     )
 }
