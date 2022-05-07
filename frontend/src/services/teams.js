4693d6d21367ef89292fde3ac5f1b1d881b4f1ba
Luca Favini <luca.favini.02@gmail.com>
2022-05-07 18:35:01 +0200

    Frontend: refactor: added team service documentation comments

--- frontend/src/services/teams.js
+++ frontend/src/services/teams.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 const baseUrl = "/api/teams"
 
+// AUTH - get every team the user is in
 const getAll = async (token) => {
     const config = {
         headers: { Authorization: `bearer ${token}` },
@@ -10,11 +11,16 @@ const getAll = async (token) => {
     return request.then(response => response.data)
 }
 
+// UNAUTH - get every team with this name
 const getTeamByName = async (name) => {
     const request = axios.get(`${baseUrl}/${name}`)
     return request.then(response => response.data)
 }
 
+
+// -------------- TEAM INVITE SYSTEM
+
+// AUTH - request to join a team, added to waitingMember list
 // id: id of the team to join
 const addWaitingMember = async (id, token) => {
     const config = {
@@ -25,6 +31,7 @@ const addWaitingMember = async (id, token) => {
     return request.then(response => response.data)
 }
 
+// AUTH - accept waiting member, move him to members (accepter should be admin)
 // userId: id of the user to accept
 // teamId: id of the team the user will be accepted to
 const acceptWaitingMember = async (userId, teamId, token) => {
@@ -36,6 +43,7 @@ const acceptWaitingMember = async (userId, teamId, token) => {
     return request.then(response => response.data)
 }
 
+// AUTH - decline waiting member, remove him from waiting members (decliner should be admin)
 // userId: id of the user to decline
 // teamId: id of the team the user will be decline to
 const declineWaitingMember = async (userId, teamId, token) => {
