49af408eecf7c6d95681acb02b1c5fb8acb24c60
Luca Favini <luca.favini.02@gmail.com>
2022-05-08 15:36:11 +0200

    Frontend: bug fixes: new team name already taken error display

--- frontend/src/components/profile/NewTeamForm.jsx
+++ frontend/src/components/profile/NewTeamForm.jsx
@@ -7,42 +7,47 @@ import JoinTeamFormStyles from "./JoinTeamForm.module.scss"
 
 const NewTeamForm = ({ user, teams, setTeams }) => {
     const [teamName, setTeamName] = useState("")
+    const [status, setStatus] = useState("Team names are unique. Spaces will be replaced by dashes (-).")
+
+    const handleTeamNameChange = (newTeamName) => {
+        setStatus("Team names are unique. Spaces will be replaced by dashes (-).")
+        setTeamName(newTeamName)
+    }
 
     const handleNewTeam = () => {
-        try {
-            teamService
-                .create(teamName, user.token)
-                .then(savedTeam => {
-                    // map the saved team as every other team already saved and mapped
-                    const newSavedTeam = [savedTeam].map(t => {
-                        return ({
-                            name: t.name,
-                            id: t.id,
-                            nMatches: t.matches.length,
-                            permission: t.members.find(m => m.id.id === user.id).permission,
-                            members: t.members.map(m => ({
-                                id: m.id.id,
-                                username: m.id.username,
-                                permission: m.permission
-                            })),
-                            waitingMembers: t.members.find(m => m.id.id === user.id).permission === "admin"
-                                ? t.waitingMembers.map(m => ({
-                                    id: m.id,
-                                    username: m.username
-                                }))
-                                : null
-                        })
-                    })[0]
+        teamService
+            .create(teamName, user.token)
+            .then(savedTeam => {
+                // map the saved team as every other team already saved and mapped
+                const newSavedTeam = [savedTeam].map(t => {
+                    return ({
+                        name: t.name,
+                        id: t.id,
+                        nMatches: t.matches.length,
+                        permission: t.members.find(m => m.id.id === user.id).permission,
+                        members: t.members.map(m => ({
+                            id: m.id.id,
+                            username: m.id.username,
+                            permission: m.permission
+                        })),
+                        waitingMembers: t.members.find(m => m.id.id === user.id).permission === "admin"
+                            ? t.waitingMembers.map(m => ({
+                                id: m.id,
+                                username: m.username
+                            }))
+                            : null
+                    })
+                })[0]
 
-                    setTeams(teams.concat(newSavedTeam).sort((t1, t2) => t1.name < t2.name ? -1 : 1))
-                })
-        }
-        catch (exception) {
-            console.log(exception)
-            if (exception.response) {
-                console.log("Error", exception.response.status, ":", exception.response.data.error)
-            }
-        }
+                setTeams(teams.concat(newSavedTeam).sort((t1, t2) => t1.name < t2.name ? -1 : 1))
+            })
+            .catch(exception => {
+                console.log(exception)
+                if (exception.response.status === 400) {
+                    setStatus("Name aldready taken")
+                    console.log("Error", exception.response.status, ":", exception.response.data.error)
+                }
+            })
     }
 
     return (
@@ -53,13 +58,13 @@ const NewTeamForm = ({ user, teams, setTeams }) => {
                 <input
                     className={JoinTeamFormStyles.teamInput}
                     type="text"
-                    onChange={({ target }) => setTeamName(target.value)}
+                    onChange={({ target }) => handleTeamNameChange(target.value)}
                     placeholder="Enter team name"
                 />
                 <button
                     className={JoinTeamFormStyles.actionButton}
                     onClick={() => handleNewTeam()}
-                >Create team</button>
+                >Create {teamName.trim().replace(" ", "-")}</button>
             </div>
         </div>
     )
