0749684bd40ec2627e3765018363da2cd32921d8
Luca Favini <luca.favini.02@gmail.com>
2022-05-08 15:44:57 +0200

    Frontend: CSS: new team and join team centered if no teams joined

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import Teams from "../components/profile/Teams"
 import NewTeamForm from "../components/profile/NewTeamForm"
 import JoinTeamForm from "../components/profile/JoinTeamForm"
 
+import classnames from "classnames"
 import ProfileStyles from "./Profile.module.scss"
 
 const Profile = ({ user, logout }) => {
@@ -16,7 +17,12 @@ const Profile = ({ user, logout }) => {
             <Header user={user} logout={logout} />
             <div className={ProfileStyles.teamsDiv}>
                 <Teams user={user} teams={teams} setTeams={setTeams}/>
-                <div className={ProfileStyles.newTeamsDiv}>
+                <div className={classnames(
+                    ProfileStyles.newTeamsDiv,
+                    teams.length === 0
+                        ? ProfileStyles.noTeams
+                        : ""
+                )}>
                     <NewTeamForm user={user} teams={teams} setTeams={setTeams}/>
                     <JoinTeamForm user={user} />
                 </div>
