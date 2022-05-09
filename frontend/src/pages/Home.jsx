d2563f13f7532a5826b8a0e1d6c5aac95e585b87
Luca Favini <luca.favini.02@gmail.com>
2022-05-09 15:38:12 +0200

    Frontend: added no match error in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 import matchService from "../services/matches"
 
 import Header from "../components/common/Header"
 import Loading from "../components/common/Loading"
 import Matches from "../components/home/Matches"
+
+import classnames from "classnames"
+import CommonStyles from "../styles/common.module.scss"
+import HomeStyles from "./Home.module.scss"
 
 const Home = ({ user, logout }) => {
     const [loading, setLoading] = useState(true)
@@ -44,13 +49,39 @@ const Home = ({ user, logout }) => {
             })
     }
 
+    // if user has no matches
+    if (matches.length === 0) {
+        return (
+            <>
+                <Header user={user} logout={logout} />
+
+                {loading && <Loading />}
+
+                {!loading &&
+                <div className={HomeStyles.noMatchesError}>
+                    <h1>You don&apos;t have any <span className={CommonStyles.highlighted}>match</span> stored :(</h1>
+                    <h3>Upload a <span className={CommonStyles.highlighted}>match</span> right now!</h3>
+                    <div className={HomeStyles.redirectDiv}>
+                        <Link
+                            to="/newmatch"
+                            className={classnames(CommonStyles.highlighLinkButton, CommonStyles.bigger)}
+                        >
+                            Upload a match now!
+                        </Link>
+                    </div>
+                </div>
+                }
+            </>
+        )
+    }
+
     return (
-        <div>
+        <>
             <Header user={user} logout={logout} />
 
             {loading && <Loading />}
             <Matches matches={matches} deleteMatch={deleteMatch} />
-        </div>
+        </>
     )
 }
 
