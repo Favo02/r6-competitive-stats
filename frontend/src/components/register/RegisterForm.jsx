97e1585e03e7096fad092d2b7cf77365662d34e9
Luca Favini <luca.favini.02@gmail.com>
2022-05-10 17:24:05 +0200

    Frontend: added register component with data validation

---
+++ frontend/src/components/register/RegisterForm.jsx
@@ -0,0 +1,153 @@
+import { useState } from "react"
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
+
+import Notification from "../common/Notification"
+
+import classnames from "classnames"
+import CommonStyles from "../../styles/common.module.scss"
+import LoginFormStyles from "../login/LoginForm.module.scss"
+
+import { FaUserAlt, FaEye, FaEyeSlash } from "react-icons/fa"
+import { MdEmail } from "react-icons/md"
+
+const RegisterForm = ({ register, notificationObj }) => {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [password2, setPassword2] = useState("")
+    const [passwordShown, setPasswordShown] = useState(false)
+
+    const handleRegister = (event) => {
+        event.preventDefault()
+        register(username, email, password, password2)
+    }
+
+    const toggleShowPassword = () => {
+        setPasswordShown(!passwordShown)
+    }
+
+    return (
+        <div className={LoginFormStyles.registerForm}>
+            <Notification notificationObj={notificationObj} />
+            <h1 className={LoginFormStyles.h1}>
+                <span
+                    className={CommonStyles.highlighted}
+                >Register </span>
+                to CompStats.
+            </h1>
+            <h3 className={LoginFormStyles.h3}>
+                Already have an account? Go to&nbsp;
+                <Link
+                    className={classnames(LoginFormStyles.link, CommonStyles.highlighted)}
+                    to="/login"
+                >Login</Link>
+            </h3>
+            <form onSubmit={handleRegister}>
+                {/* Username */}
+                <div className={LoginFormStyles.div}>
+                    <label
+                        className={LoginFormStyles.label}
+                        htmlFor="Username"
+                    >Username</label>
+                    <input
+                        className={LoginFormStyles.input}
+                        id="username-input"
+                        type="text"
+                        value={username}
+                        name="Username"
+                        placeholder="Grande Puffo"
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                    <h4 className={LoginFormStyles.description}>Username is unique and should be min 4, max characters, alphanumeric, dash and dot (- .) allowed. Special characters can&apos;t be at start or end and can&apos;t be consecutive</h4>
+                    <FaUserAlt
+                        className={LoginFormStyles.icon}
+                    />
+                </div>
+                {/* Email */}
+                <div className={LoginFormStyles.div}>
+                    <label
+                        className={LoginFormStyles.label}
+                        htmlFor="Email"
+                    >Email</label>
+                    <input
+                        className={LoginFormStyles.input}
+                        id="email-input"
+                        type="text"
+                        value={email}
+                        name="Email"
+                        placeholder="grandepuffo@pmail.com"
+                        onChange={({ target }) => setEmail(target.value)}
+                    />
+                    <MdEmail
+                        className={LoginFormStyles.icon}
+                    />
+                </div>
+                {/* Password */}
+                <div className={LoginFormStyles.div}>
+                    <label
+                        className={LoginFormStyles.label}
+                        htmlFor="Password"
+                    >Password</label>
+                    <input
+                        className={LoginFormStyles.input}
+                        id="password-input"
+                        type={passwordShown ? "text" : "password"}
+                        value={password}
+                        name="Password"
+                        placeholder={passwordShown ? "Pa$$w0rD" : "●●●●●●●●"}
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                    <h4 className={LoginFormStyles.description}>Password should be min 8, max 16 characters, at least one letter, one number and one special character (@$!%*#?&)</h4>
+                    { passwordShown
+                        ? <FaEyeSlash
+                            className={classnames(CommonStyles.pointer, LoginFormStyles.icon)}
+                            onClick={toggleShowPassword}
+                        />
+                        : <FaEye
+                            className={classnames(CommonStyles.pointer, LoginFormStyles.icon)}
+                            onClick={toggleShowPassword}
+                        />
+                    }
+                </div>
+                {/* Confirm password */}
+                <div className={LoginFormStyles.div}>
+                    <label
+                        className={LoginFormStyles.label}
+                        htmlFor="Password2"
+                    >Confirm password</label>
+                    <input
+                        className={LoginFormStyles.input}
+                        id="password2-input"
+                        type={passwordShown ? "text" : "password"}
+                        value={password2}
+                        name="Password2"
+                        placeholder={passwordShown ? "Pa$$w0rD" : "●●●●●●●●"}
+                        onChange={({ target }) => setPassword2(target.value)}
+                    />
+                    { passwordShown
+                        ? <FaEyeSlash
+                            className={classnames(CommonStyles.pointer, LoginFormStyles.icon)}
+                            onClick={toggleShowPassword}
+                        />
+                        : <FaEye
+                            className={classnames(CommonStyles.pointer, LoginFormStyles.icon)}
+                            onClick={toggleShowPassword}
+                        />
+                    }
+                </div>
+                <button
+                    className={LoginFormStyles.loginButton}
+                    type="submit"
+                    id="register-button"
+                >Register</button>
+            </form>
+        </div>
+    )
+}
+
+RegisterForm.propTypes = {
+    register: PropTypes.func.isRequired
+}
+
+export default RegisterForm
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import jwt_decode from "jwt-decode"
+
+import usersService from "../services/users"
+import loginService from "../services/login"
+
+import RegisterForm from "../components/register/RegisterForm"
+
+import RegisterStyles from "./Register.module.scss"
+
+const Register = ({ setUser, notification, notificate }) => {
+
+    const register = async (username, email, password, password2) => {
+
+        if (! (/^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){2,14}[a-zA-Z0-9]$/.test(username))) {
+            notificate("Enter a valid username", true)
+            return
+        }
+
+        if (! (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            notificate("Enter a valid email", true)
+            return
+        }
+
+        if (password !== password2) {
+            notificate("The passwords don't match", true)
+            return
+        }
+
+        if (! (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/.test(password))) {
+            notificate("Enter a valid password", true)
+            return
+        }
+
+        try {
+            await usersService.create(username, email, password)
+            console.log("user created")
+            const user = await loginService.login({ username, password })
+            console.log(user, " login")
+
+            const decoded = jwt_decode(user.token)
+            user.id = decoded.id
+
+            notificate(`${user.username} logged in`, false)
+
+            window.localStorage.setItem(
+                "loggedR6statsUser", JSON.stringify(user)
+            )
+            setUser(user)
+        }
+        catch (exception) {
+            console.log(exception)
+            if (exception.response.data.error === "email taken") {
+                notificate("This email is already used, try recoved password", true)
+                return
+            }
+            if (exception.response.data.error === "username taken") {
+                notificate("This username is already used", true)
+                return
+            }
+            if (exception.response) {
+                notificate(`Login error: code ${exception.response.status} - ${exception.response.data.error}`, true)
+            }
+        }
+    }
+
+    return (
+        <div className={RegisterStyles.background}>
+            <RegisterForm register={register} notificationObj={notification} />
+        </div>
+    )
+}
+
+export default Register
